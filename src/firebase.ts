7449894ad4c66a5be5033162e9f32bb1ef7b7717
«wctnz» <wctnzz@gmail.com>
2024-03-05 17:41:01 +0300

    apiKey

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'mock_key',
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
